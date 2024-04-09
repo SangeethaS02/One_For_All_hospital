93659a6d4921b649539078e08dcbd7d585089e7f
SangeethaS02 <162785848+SangeethaS02@users.noreply.github.com>
2024-04-09 12:12:27 +0530

    Add files via upload

---
+++ ONE_FOR_ALL/ONE_FOR_ALL/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ONE_FOR_ALL project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ONE_FOR_ALL.settings')
+
+application = get_asgi_application()
